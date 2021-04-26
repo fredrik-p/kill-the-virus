6edecc014cf494909494b20d04072f8200eb579e
Fredrik P <fredrik.pekkarinen@gmail.com>
2021-04-26 14:32:19 +0200

    Socket connection and some css

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -4,3 +4,17 @@
 
 const debug = require('debug')('kill-the-virus:socket_controller');
 
+const players = {}
+
+const handleNewPlayer = function (username) {
+    players[this.id] = username;
+    debug(players)
+}
+
+
+module.exports = function (socket) {
+    debug(`Client ${socket.id} connected!`);
+
+    socket.on('newPlayer', handleNewPlayer)
+
+}
--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,3 +1,20 @@
 /**
  * Game
  */
+
+var socket = io();
+
+console.log('game');
+
+// Listen for when the player form is submited
+document.querySelector('#player-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    console.log('Clicked!')
+
+    // Get the value from the input
+    const username = document.querySelector('#username').value
+
+    socket.emit('newPlayer', username)
+
+});
